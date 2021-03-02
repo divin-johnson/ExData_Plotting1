de024a9c8e84090c10ed2a65ce96918b71eb51de
divin-johnson <72494712+divin-johnson@users.noreply.github.com>
2021-03-02 19:09:58 +0530

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -8,6 +8,7 @@ subdata <- data.frame(c(Date_and_Time,subdata))
 subdata[,1] <- as.POSIXct(subdata[,1], "%Y-%m-%d %H:%M:%S")
 
 #The third plot requires 3 different lines representing 3 different variables from the dataframe in a single plot
+png(file = 'plot1.R') #the default setting of this command is height and width as 480 in pixels format
 #Making an empty canvas for plot using command type = 'n' 
 plot(subdata$Date_and_Time,as.numeric(subdata$Sub_metering_1), type = 'n', xlab = "", ylab = "Energy sub metering")
 #adding lines to the empty canvas plot for the required three variables with 3 different colors as asked
@@ -16,3 +17,4 @@ lines(subdata$Date_and_Time,as.numeric(subdata$Sub_metering_2),col = "red")
 lines(subdata$Date_and_Time,as.numeric(subdata$Sub_metering_3),col = "blue")
 #adding an information section to the topright of the plot that identifies each variable by its distinctive color 
 legend('topright',col = c('black', 'red', 'blue'), lty = 1, legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3") )
+dev.off()
